b58668aebf87928ad6d1eaa4a80f9be4b11de8e6
Cintia <cindysac.2020@gmail.com>
2024-04-26 20:53:27 -0400

    operadores de asignacion

---
+++ valores_de_entrada.py
@@ -0,0 +1,14 @@
+mi_variable=input ('escribe tu valor:')
+print ('tu valor es:'+ mi_variable)
+print (type (mi_variable))
+
+a= input('primer numero :')
+b= input('segundo numero :')
+c= int(input('segundo numero :'))
+resultado =a+b
+print (resultado)
+
+a_transformado=int(a)
+b=int(b)
+resultado=a_transformado+b+c
+print('resultado :'+str(resultado))
